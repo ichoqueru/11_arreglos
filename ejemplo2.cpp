881cad1de59ce9bbd96c536df718fa6c40b0b5aa
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-12 00:48:52 -0500

    Actualizar ejemplo2: verificar si el número se encuentre entre 1 y 20

--- ejemplo2.cpp
+++ ejemplo2.cpp
@@ -4,6 +4,10 @@ int main(){
 	int n,A[20],s,i,prom;
 	cout<<"Ingrese la cantidad de elemntos: "; cin>>n;
 	s=0;
+    while(n<=0 || n>20){
+        cout<<"El numero debe ser mayor que 0 y menor o igual a 20"<<endl;
+        cout<<"Inténtelo de nuevo"<<endl; cin>>n;
+    }
 	for(i=0;i<n;i++){
 		cout<<"Ingrese un numero: "; cin>>A[i];
 		s=s+A[i];
