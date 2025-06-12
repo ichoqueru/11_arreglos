4bd46759e81564d6a972d98690eb55beaed69f3e
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-12 00:39:45 -0500

    Actualizar ejemplo1: agrega condición para ser positivo y menor o igual a 20

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int main(){
     int m,i,A[20],max;
     cout<<"Ingrese la cantidad de elementos: "; cin>>m;
+    while(m<=0 || m>20){
+        cout<<"El numero debe ser mayor que 0 y menor o igual a 20"<<endl;
+        cout<<"Ingrese otro numero: ";
+        cin>>m;
+    }
     for (i=0;i<m;i++){
         cout<<"Ingrese un numero: "; cin>>A[i];
     }
@@ -11,7 +16,6 @@ int main(){
         if(A[i]>max){
             max=A[i];
         }
-    }
     cout<<"El numero mayor es: "<<max;
     return 0;
 }
