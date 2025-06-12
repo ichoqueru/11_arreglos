9b34af494593d19a55b9de8ab5dbb5c586245aab
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-12 00:50:56 -0500

    Actualizar ejemplo3: añade condición de intervalo de 1 a 20

--- ejemplo3.cpp
+++ ejemplo3.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main(){
 	int n,i,A[20],aux;
 	cout<<"Ingrese una cantidad de elementos: "; cin>>n;
+    while(n<=0 || n>20){
+        cout<<"El numero debe ser mayor que 0 y menor o igual a 20"<<endl;
+        cout<<"Inténtelo de nuevo"<<endl; cin>>n;
+    }
 	for(i=0;i<n;i++){
 		cout<<"Ingrese un numero: "; cin>>A[i];
 	}
