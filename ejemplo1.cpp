eea55db22c2a88e29fef3153e84375ff4d8a7737
yngomeza <yngomeza@unjbg.edu.pe>
2025-07-09 22:02:20 -0500

    Actualizar ejemplo 1: Agregar calculo del número mayor

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -1,21 +1,37 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int m,i,A[20],max;
-    cout<<"Ingrese la cantidad de elementos: "; cin>>m;
-    while(m<=0 || m>20){
-        cout<<"El numero debe ser mayor que 0 y menor o igual a 20"<<endl;
-        cout<<"Ingrese otro numero: ";
-        cin>>m;
+    int m, i, A[20], max, min;
+
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> m;
+
+    while(m <= 0 || m > 20){
+        cout << "El numero debe ser mayor que 0 y menor o igual a 20" << endl;
+        cout << "Ingrese otro numero: ";
+        cin >> m;
     }
-    for (i=0;i<m;i++){
-        cout<<"Ingrese un numero: "; cin>>A[i];
+
+    for (i = 0; i < m; i++){
+        cout << "Ingrese un numero: ";
+        cin >> A[i];
     }
-    max=A[0];
-    for(i=1;i<m;i++){
-        if(A[i]>max){
-            max=A[i];
+
+    max = A[0];
+    min = A[0];
+
+    for(i = 1; i < m; i++){
+        if(A[i] > max){
+            max = A[i];
         }
-    cout<<"El numero mayor es: "<<max;
+        if(A[i] < min){
+            min = A[i];
+        }
+    }
+
+    cout << "El numero mayor es: " << max << endl;
+    cout << "El numero menor es: " << min << endl;
+
     return 0;
 }
